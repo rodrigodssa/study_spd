5d7c98f1b141554210d0a11f860060f4a0a57830
rodrigodssa <rodrigodssalmeida@gmail.com>
2024-06-18 19:38:32 -0300

    novo commit

---
+++ exercicios/primo.py
@@ -0,0 +1,9 @@
+def eh_primo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero ** 0.5) + 1):
+        if numero % i == 0:
+            return False
+    return True
+numero = int(input("Digite um número: "))
+print(f"O número {numero} é primo? {eh_primo(numero)}")
